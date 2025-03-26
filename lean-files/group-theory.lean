92b0fa0968be4cecd5e7a554747fea79ccd30c96
gulatiaabhas <160495872+gulatiaabhas@users.noreply.github.com>
2025-03-26 15:23:21 +0530

    Create group-theory.lean

---
+++ lean-files/group-theory.lean
@@ -0,0 +1,42 @@
+import Mathlib
+
+-- Proof 1 of this theorem
+
+theorem group_abelian (G : Type*) [Group G] (h : ∀ x : G, x * x = 1) : (∀ x y : G , x * y = y * x) := by 
+
+intro x y 
+have hxy := h (x*y)
+have hyx := h (y*x)
+
+have hxx := h x 
+have hyy := h y
+have mul : x * (x * y * (x * y)) = x * 1 := by rw[hxy]
+repeat rw[← mul_assoc] at mul
+
+rw[hxx] at mul
+rw[one_mul] at mul
+rw[mul_one] at mul
+
+
+have new_mul : y * (y * x * y) = y * x := by rw[mul]
+repeat rw[← mul_assoc] at new_mul
+
+rw[hyy] at new_mul
+rw[one_mul] at new_mul
+exact new_mul
+
+-- Proof 2 of this theorem
+
+theorem group_abelian_2 (G : Type*) [Group G] (h : ∀ x : G, x * x = 1) : (∀ x y : G , x * y = y * x) := by 
+
+intro x y 
+have hxy := h (x*y)
+have hyx := h (y*x)
+
+have hxx := h x 
+have hyy := h y
+
+have mul : (x * x) * y * (x * y) = x * 1 :=  
+calc 
+(x * x) * y * (x * y) = x * (x * y * (x * y)) := by group
+x * (x * y * (x * y)) = x * 1 := by rw[hxy]
